a17c36ebe1b5f612ffe83234f57fffb7b2d5b52f
jezyk_we_mgle <anastasiafabricnaama@gmail.com>
2023-05-20 14:47:44 +0300

    structure

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 from flask import Flask
+from faker import Faker
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/requirements/')
 def requirements():
     with open('requirements.txt', 'r') as file:
         out = file.read().replace('\n', '<br>')
     return f"Requirements:<br> {out}"
+
+
+@app.route('/generate-users/')
+def generate_users():
+
+    return ''
+
+
+@app.route('/mean/')
+def mean():
+    return ''
+
+
+@app.route('/space/')
+def space():
+    return ''
 
 
 if __name__ == '__main__':
